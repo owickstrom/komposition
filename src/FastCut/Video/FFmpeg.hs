08be923d78da954c821c349b93f6a3b016f30cd9
Oskar Wickström <oskar.wickstrom@gmail.com>
2018-05-23 21:45:52 +0200

    Read with time and color code output

--- src/FastCut/Video/FFmpeg.hs
+++ src/FastCut/Video/FFmpeg.hs
@@ -6,31 +6,41 @@ module FastCut.Video.FFmpeg where
 import           Codec.FFmpeg
 import           Codec.FFmpeg.Encode
 import           Codec.Picture
-import           Control.Lens
+import           Codec.Picture.Types
 import           Control.Monad.IO.Class
-import           Data.Bits
-import qualified Data.Vector.Storable   as Vector
-import           Pipes                  (Consumer', Pipe, Producer)
-import           Pipes                  as Pipes
-import           Pipes.Extras           (scan1)
-import qualified Pipes.Prelude          as Pipes
+import           Control.Monad.Primitive
+import qualified Data.Vector.Storable    as Vector
+import           Data.Word
+import           Debug.Trace
+import           Pipes                   (Consumer', Pipe, Producer)
+import           Pipes                   as Pipes
+import           Pipes.Extras            (scan1M)
+import qualified Pipes.Prelude           as Pipes
 import           System.Directory
 import           System.FilePath
+import           System.IO
+import           Text.Printf
 
 initialize :: IO ()
 initialize = initFFmpeg
 
 type Frame = Image PixelRGB8
+type TimedFrame = (Frame, Double)
 
-readVideoFile :: MonadIO m => FilePath -> Producer Frame m ()
+readVideoFile :: MonadIO m => FilePath -> Producer TimedFrame m ()
 readVideoFile path = do
-  (getFrame, cleanup) <- liftIO (imageReader (File path))
+  (getFrame, cleanup) <- liftIO (imageReaderTime (File path))
   yieldNext getFrame cleanup
   where
+    yieldNext ::
+         MonadIO m
+      => IO (Maybe TimedFrame)
+      -> IO end
+      -> Producer TimedFrame m end
     yieldNext getFrame cleanup = do
       m <- liftIO getFrame
       case m of
-        Just f  -> do
+        Just f -> do
           Pipes.yield f
           yieldNext getFrame cleanup
         Nothing -> liftIO cleanup
@@ -42,66 +52,98 @@ writeVideoFile path source = do
   Pipes.runEffect $ Pipes.for source (liftIO . writeFrame . Just)
   liftIO (writeFrame Nothing)
 
-printFrames :: MonadIO m => Consumer' Frame m ()
-printFrames = do
-  f <- Pipes.await
-  liftIO (Prelude.print (Vector.length (imageData f)))
-  printFrames
+dropTime :: Monad m => Pipe TimedFrame Frame m ()
+dropTime = Pipes.map fst
 
 equalFrame :: Frame -> Frame ->  Bool
 equalFrame a b = Vector.and (Vector.zipWith (==) (imageData a) (imageData b))
 
 equalFrame2 :: Int -> Frame -> Frame ->  Bool
-equalFrame2 skip a b = la == lb && go 0
+equalFrame2 skip a b = la == lb && sim > 0.95
   where
     da = imageData a
     db = imageData b
     la = Vector.length da
     lb = Vector.length db
-    go i
-      | i >= la = True
-      | otherwise = Vector.unsafeIndex da i == Vector.unsafeIndex db i && go (i + skip)
+    go i (eq, neq)
+      | i >= la = (eq, neq)
+      | otherwise =
+        if Vector.unsafeIndex da i == Vector.unsafeIndex db i
+        then go (i + skip) (succ eq, neq)
+        else go (i + skip) (eq, succ neq)
+    sim :: Double
+    sim =
+      let (eq, neq) = go 0 (0, 0)
+      in eq / (eq + neq)
 
 equalFrameCountThreshold :: Int
-equalFrameCountThreshold = 60
+equalFrameCountThreshold = 15
 
 data MovementFrame
   = Moving Frame
   | Still Frame
 
+instance Show MovementFrame where
+  show mf =
+    case mf of
+      Moving _ -> "Moving <frame>"
+      Still _  -> "Still <frame>"
+
 data ClassifierState
   = InMoving { lastFrame       :: !Frame
              , equalFrameCount :: !Int }
-  | InStill { stillFrame :: !Frame }
+  | InStill { stillFrame :: !Frame, lastFrame :: !Frame }
 
 
 classifyMovement :: MonadIO m => Pipe Frame MovementFrame m ()
-classifyMovement = scan1 next first toMovementFrame
+classifyMovement = scan1M nextFrame (pure . firstFrame) (pure . toMovementFrame)
   where
-    first frame = InMoving frame 1
-    next state frame =
+    firstFrame frame = InMoving frame 1
+    nextFrame state frame =
       case state of
         InMoving {..}
-          | equalFrame2 32 frame lastFrame ->
-            if succ equalFrameCount > equalFrameCountThreshold
-              then InStill frame
-              else InMoving frame (succ equalFrameCount)
-          | otherwise -> InMoving frame 1
+          | equalFrame2 1 frame lastFrame ->
+            if equalFrameCount > equalFrameCountThreshold
+              then pure (InStill frame frame)
+              else pure (InMoving frame (succ equalFrameCount))
+          | otherwise -> pure (InMoving frame 1)
         InStill {..}
-          | equalFrame2 32 stillFrame frame -> InStill stillFrame
-          | otherwise -> InMoving frame 1
+          | equalFrame2 1 stillFrame frame -> pure (InStill stillFrame frame)
+          | otherwise -> pure (InMoving frame 1)
     toMovementFrame =
       \case
         InMoving {..} -> Moving lastFrame
-        InStill {..} -> Still stillFrame
+        InStill {..} -> Still lastFrame
 
-colorClassifiedMovement :: MonadIO m => Pipe MovementFrame Frame m ()
+colorClassifiedMovement :: (PrimMonad m, MonadIO m) => Pipe MovementFrame Frame m ()
 colorClassifiedMovement =
-  Pipes.map $ \case
-    Moving frame -> runIdentity (traverseOf imagePixels (tint 0x0000FF) frame)
-    Still frame -> runIdentity (traverseOf imagePixels (tint 0xFF0000) frame)
+  Pipes.mapM $ \case
+    Moving frame -> tint' green frame
+    Still frame -> tint' red frame
   where
-    tint color = pure . colorMap (.&. color)
+    tint' color frame = do
+      mf <- thawImage frame
+      tint color mf
+      freezeImage mf
+    red = PixelRGB8 255 0 0
+    green = PixelRGB8 0 255 0
+
+blend :: PixelRGB8 -> PixelRGB8 -> PixelRGB8
+blend = mixWith (const meanWord8)
+
+meanWord8 :: Word8 -> Word8 -> Word8
+meanWord8 a b =
+  let n = fromIntegral a + fromIntegral b :: Word16
+  in fromIntegral (n `div` 2)
+
+tint :: (PrimMonad m) => PixelRGB8 -> MutableImage (PrimState m) PixelRGB8 -> m ()
+tint color frame =
+  sequence_
+  [ do p <- readPixel frame x y
+       writePixel frame x y (blend color p)
+  | x <- [0 .. pred (mutableImageWidth frame)]
+  , y <- [0 .. pred (mutableImageHeight frame)]
+  ]
 
 -- data SplitterState
 --   = Initial
@@ -156,7 +198,24 @@ colorClassifiedMovement =
 --   let ep = defaultH264 1920 1080
 --   in imageWriter ep (outDir </> Prelude.show n <> ".mp4")
 
+printProcessingInfo :: MonadIO m => Consumer' TimedFrame m ()
+printProcessingInfo =
+  Pipes.mapM_ $ \(_, n) ->
+    liftIO $ do
+      let s = floor n :: Int
+      printf
+        "Processing at %02d:%02d:%02d\r"
+        (s `div` 3600)
+        (s `div` 60)
+        (s `mod` 60)
+      hFlush stdout
+
 split :: FilePath -> FilePath -> IO ()
-split from outDir = do
+split src outDir = do
   createDirectoryIfMissing True outDir
-  writeVideoFile (outDir </> "colored.mp4") (readVideoFile from >-> classifyMovement >-> colorClassifiedMovement)
+  writeVideoFile (outDir </> "debug.mp4") $
+    readVideoFile src
+    >-> Pipes.tee printProcessingInfo
+    >-> dropTime
+    >-> classifyMovement
+    >-> colorClassifiedMovement
