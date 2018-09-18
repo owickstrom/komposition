61c1f03ae37e576dbeb3f549e41aabe12f797c40
Oskar Wickström <oskar.wickstrom@gmail.com>
2018-09-18 18:09:24 +0200

    Comment unused juicy images

--- bench/Benchmark.hs
+++ bench/Benchmark.hs
@@ -26,8 +26,8 @@ readMassivTestImage = Massiv.readImage . testImageName
 
 main :: IO ()
 main = do
-  jimg1 <- readJuicyTestImage 1
-  jimg2 <- readJuicyTestImage 2
+  -- jimg1 <- readJuicyTestImage 1
+  -- jimg2 <- readJuicyTestImage 2
   himg1 <- readMassivTestImage 1
   himg2 <- readMassivTestImage 2
   defaultMain
