959fd645a8560efe9899ae2d4a8c1e1e729144b7
Oskar Wickström <oskar.wickstrom@gmail.com>
2018-10-19 19:38:49 +0200

    Use CustomWidget for thumbnail preview

--- src/Komposition/UserInterface/GtkInterface/ThumbnailPreview.hs
+++ src/Komposition/UserInterface/GtkInterface/ThumbnailPreview.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE LambdaCase        #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 module Komposition.UserInterface.GtkInterface.ThumbnailPreview where
 
@@ -12,44 +10,37 @@ import qualified GI.Gtk                         as Gtk
 import           GI.Gtk.Declarative
 import           GI.Gtk.Declarative.EventSource
 
-data ThumbnailPreview event = ThumbnailPreview
-  { thumbnailPath :: FilePath
-  }
+import Komposition.UserInterface.GtkInterface.CustomWidget
 
 thumbnailPreview :: FilePath -> Widget a
-thumbnailPreview thumbnailPath =
-  Widget ThumbnailPreview {..}
+thumbnailPreview customData = Widget (CustomWidget {..})
+  where
+    customWidget = Gtk.Layout
+    customCreate thumbnailPath = do
+      layout <- Gtk.layoutNew Gtk.noAdjustment Gtk.noAdjustment
+      image <- Gtk.imageNewFromPixbuf Pixbuf.noPixbuf
+      Gtk.widgetSetSizeRequest layout 200 200
+      Gtk.layoutPut layout image 0 0
+      let redraw w h = void . async $ do
+            scaled <- Pixbuf.pixbufNewFromFileAtSize thumbnailPath w h
+            void . Gdk.threadsAddIdle GLib.PRIORITY_DEFAULT $ do
+              Gtk.imageSetFromPixbuf image (Just scaled)
+              return False
+      void . Gtk.onWidgetMapEvent layout $ \_ -> do
+        (_, r) <- Gtk.widgetGetPreferredSize layout
+        w <- Gtk.getRequisitionWidth r
+        h <- Gtk.getRequisitionHeight r
+        redraw w h
+        return True
+      void . Gtk.onWidgetSizeAllocate layout $ \a -> do
+        w <- Gdk.getRectangleWidth a
+        h <- Gdk.getRectangleHeight a
+        redraw w h
+      return layout
 
-instance Functor ThumbnailPreview where
-  fmap _ ThumbnailPreview {..} = ThumbnailPreview {..}
+    customPatch old new
+      | old == new = CustomKeep
+      | otherwise = CustomReplace
 
-instance Patchable ThumbnailPreview where
-  create ThumbnailPreview {..} = do
-    layout <- Gtk.layoutNew Gtk.noAdjustment Gtk.noAdjustment
-    image <- Gtk.imageNewFromPixbuf Pixbuf.noPixbuf
-    Gtk.widgetSetSizeRequest layout 200 200
-    Gtk.layoutPut layout image 0 0
-    let redraw w h = void . async $ do
-          scaled <- Pixbuf.pixbufNewFromFileAtSize thumbnailPath w h
-          void . Gdk.threadsAddIdle GLib.PRIORITY_DEFAULT $ do
-            Gtk.imageSetFromPixbuf image (Just scaled)
-            return False
-    void . Gtk.onWidgetMapEvent layout $ \_ -> do
-      (_, r) <- Gtk.widgetGetPreferredSize layout
-      w <- Gtk.getRequisitionWidth r
-      h <- Gtk.getRequisitionHeight r
-      redraw w h
-      return True
-    void . Gtk.onWidgetSizeAllocate layout $ \a -> do
-      w <- Gdk.getRectangleWidth a
-      h <- Gdk.getRectangleHeight a
-      redraw w h
-    Gtk.toWidget layout
-  patch old new 
-    | thumbnailPath old == thumbnailPath new = Keep
-    | otherwise = Replace (create new)
-
-instance EventSource ThumbnailPreview where
-  subscribe ThumbnailPreview{..} _ _ = do
-    -- TODO: Implement event source
-    return (fromCancellation (return ()))
+    customSubscribe _ _ _ =
+      return (fromCancellation (return ()))
