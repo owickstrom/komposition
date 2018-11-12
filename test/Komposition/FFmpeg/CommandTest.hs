eae8f28941c0145b51f7a35ea9b6ca79c349b015
Oskar Wickström <oskar.wickstrom@gmail.com>
2018-11-12 09:01:16 +0100

    Adjust ffmpeg command render test

--- test/Komposition/FFmpeg/CommandTest.hs
+++ test/Komposition/FFmpeg/CommandTest.hs
@@ -14,7 +14,8 @@ import           Komposition.FFmpeg.Command
 spec_printCommandLineArgs = do
   it "prints command-line args for a multi-chain concat command" $
     let videoStream = StreamSelector (StreamName "video") Nothing Nothing
-        audioStream = StreamSelector (StreamName "audio") Nothing Nothing
+        audioStream1 = StreamSelector (StreamName "audio1") Nothing Nothing
+        audioStream2 = StreamSelector (StreamName "audio2") Nothing Nothing
         filter1a =
           RoutedFilter
             [ StreamSelector (StreamIndex 0) (Just Video) (Just 0)
@@ -28,16 +29,17 @@ spec_printCommandLineArgs = do
             [StreamSelector (StreamIndex 2) (Just Audio) (Just 0)]
             (Concat 1 0 1)
             []
-        filter2b = RoutedFilter [] AudioSetPTSStart [audioStream]
+        filter2b = RoutedFilter [] AudioSetPTSStart [audioStream1]
+        filter3a = RoutedFilter [] (AudioEvalSource 4) [audioStream2]
         chain1 = FilterChain (filter1a :| [filter1b])
         chain2 = FilterChain (filter2a :| [filter2b])
-        filterGraph = Just (FilterGraph (chain1 :| [chain2]))
+        chain3 = FilterChain (pure filter3a)
+        filterGraph = Just (FilterGraph (chain1 :| [chain2, chain3]))
         inputs =
           (FileSource "foo.mp4" :|
            [ StillFrameSource "bar.png" 25 10
-           , AudioNullSource (durationFromSeconds 4.3)
            ])
-        mappings = [videoStream, audioStream]
+        mappings = [videoStream, audioStream1, audioStream2]
         format = Just "mp4"
         output = FileOutput "bar.mp4"
         frameRate = Nothing
@@ -55,16 +57,14 @@ spec_printCommandLineArgs = do
        , "00:00:10.0"
        , "-i"
        , "bar.png"
-       , "-f"
-       , "lavfi"
-       , "-i"
-       , "aevalsrc=0:duration=4.3"
        , "-filter_complex"
-       , "[0:v:0][1:v:0]concat=n=2:v=1:a=0,setpts=PTS-STARTPTS[video];[2:a:0]concat=n=1:v=0:a=1,asetpts=PTS-STARTPTS[audio]"
+       , "[0:v:0][1:v:0]concat=n=2:v=1:a=0,setpts=PTS-STARTPTS[video];[2:a:0]concat=n=1:v=0:a=1,asetpts=PTS-STARTPTS[audio1];aevalsrc=0:duration=4.0[audio2]"
        , "-map"
        , "[video]"
        , "-map"
-       , "[audio]"
+       , "[audio1]"
+       , "-map"
+       , "[audio2]"
        , "-f"
        , "mp4"
        , "bar.mp4"
