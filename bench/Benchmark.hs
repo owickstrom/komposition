02eb13de82a3f479bf4ad54d03d55218fba2dfc5
Oskar Wickström <oskar.wickstrom@gmail.com>
2019-01-16 19:18:51 +0100

    Add benchmarks for timeline insert

--- bench/Benchmark.hs
+++ bench/Benchmark.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
@@ -7,6 +8,14 @@ import qualified Codec.Picture                   as Juicy
 import           Criterion.Main
 import qualified Data.ByteString                 as ByteString
 import qualified Data.Massiv.Array.IO            as Massiv
+import           Komposition.Composition
+import           Komposition.Composition.Focused
+import           Komposition.Composition.Insert
+import           Komposition.Duration
+import           Komposition.Focus
+import           Komposition.Library
+import           Komposition.MediaType
+import           Komposition.VideoSpeed
 
 import           Komposition.Import.Video.FFmpeg
 
@@ -24,6 +33,27 @@ readJuicyTestImage n = do
 readMassivTestImage :: Int -> IO MassivFrame
 readMassivTestImage = Massiv.readImage . testImageName
 
+singleParallelTimelineWithClips :: Int -> Timeline ()
+singleParallelTimelineWithClips n = Timeline
+  (pure (Sequence () (pure (Parallel () (replicate n videoClip) []))))
+
+videoClip :: VideoPart ()
+videoClip = VideoClip
+    ()
+    (VideoAsset (AssetMetadata (OriginalPath "1.mp4") 4)
+                (TranscodedPath "1.transcoded.mp4")
+                (TranscodedPath "1.proxy.mp4")
+                (VideoSpeed 1.0)
+                Nothing
+    )
+    (TimeSpan 0 4)
+    (VideoSpeed 1)
+
+clipFocus :: MediaType -> Int -> Focus SequenceFocusType
+clipFocus mt i =
+  SequenceFocus 0 (Just (ParallelFocus 0 (Just (ClipFocus mt i))))
+
+
 main :: IO ()
 main = do
   -- jimg1 <- readJuicyTestImage 1
@@ -32,10 +62,25 @@ main = do
   himg2 <- readMassivTestImage 2
   defaultMain
     [ bgroup
-        "frame equality check"
-        [ bench "equalFrame(1)" $ whnf (equalFrame 1  0.999 himg1) himg1
-        , bench "!equalFrame(1)" $ whnf (equalFrame 1  0.999 himg1) himg2
-        , bench "equalFrame'(32)" $ whnf (equalFrame' 1 0.99 himg1) himg1
-        , bench "!equalFrame'(32)" $ whnf (equalFrame' 1  0.99 himg1) himg2
-        ]
+      "frame equality check"
+      [ bench "equalFrame(1)" $ whnf (equalFrame 1 0.999 himg1) himg1
+      , bench "!equalFrame(1)" $ whnf (equalFrame 1 0.999 himg1) himg2
+      , bench "equalFrame'(32)" $ whnf (equalFrame' 1 0.99 himg1) himg1
+      , bench "!equalFrame'(32)" $ whnf (equalFrame' 1 0.99 himg1) himg2
+      ]
+    , bgroup
+      "timeline insertion"
+      [ bench "at beginning" $ whnf
+        (insert_ (clipFocus Video 0) (InsertVideoParts [videoClip]) LeftOf)
+        (singleParallelTimelineWithClips 10000)
+      , bench "at end" $ whnf
+        (insert_ (clipFocus Video 9999) (InsertVideoParts [videoClip]) RightOf)
+        (singleParallelTimelineWithClips 10000)
+      ]
+    , bgroup
+      "withAllFoci"
+      [ bench "10000" $ whnf
+        withAllFoci
+        (singleParallelTimelineWithClips 10000)
+      ]
     ]
