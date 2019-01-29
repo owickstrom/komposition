8a78ccaf1a2931fde2a66d7328b908dcee761ae8
Oskar Wickström <oskar.wickstrom@gmail.com>
2019-01-29 23:36:28 +0100

    Include current state in undo/redo History type

--- src/Komposition/UndoRedo.hs
+++ src/Komposition/UndoRedo.hs
@@ -6,7 +6,6 @@
 {-# LANGUAGE KindSignatures        #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns        #-}
-{-# LANGUAGE TupleSections         #-}
 {-# LANGUAGE TypeFamilies          #-}
 module Komposition.UndoRedo where
 
@@ -27,64 +26,65 @@ data Direction = Forward | Backward
 newtype Directed (dir :: Direction) action =
   Directed { unDirected :: action}
 
-invertDirected :: Directed dir action -> Directed (Flip dir) action
-invertDirected (Directed action) = Directed action
+invertDirected
+  :: Invertible action => Directed dir action -> Directed (Flip dir) action
+invertDirected (Directed action) = Directed (invert action)
 
 type family Flip (d :: Direction) where
   Flip Forward = Backward
   Flip Backward = Forward
 
-data History action = History
-  { toUndo :: [Directed Backward action]
-  , toRedo :: [Directed Forward action]
+data History action state = History
+  { current :: state
+  , toUndo  :: [Directed Backward action]
+  , toRedo  :: [Directed Forward action]
   }
 
-init :: History action
-init = History mempty mempty
+init :: state -> History action state
+init current = History current mempty mempty
 
 -- TODO: track size in structure instead of counting lists
-size :: History action -> Int
+size :: History action state -> Int
 size History { toUndo, toRedo } = length toUndo + length toRedo
 
-numUndos :: History action -> Int
+numUndos :: History action state -> Int
 numUndos History { toUndo } = length toUndo
 
-numRedos :: History action -> Int
+numRedos :: History action state -> Int
 numRedos History { toRedo } = length toRedo
 
 runAndRecord
-  :: (Functor f, Invertible action, Runnable action subject f)
+  :: (Functor f, Invertible action, Runnable action state f)
   => Directed Forward action
-  -> subject
-  -> History action
-  -> f (History action, subject)
-runAndRecord action subject history =
-  (history { toUndo = invertDirected action : toUndo history }, )
-    <$> run (unDirected action) subject
+  -> History action state
+  -> f (History action state)
+runAndRecord action history =
+  run (unDirected action) (current history) <&> \c ->
+    history { current = c, toUndo = invertDirected action : toUndo history }
 
 undo
-  :: (Runnable action subject m, Invertible action)
-  => History action
-  -> subject
-  -> Maybe (History action, m subject)
-undo history subject = do
+  :: (Functor f, Runnable action state f, Invertible action)
+  => History action state
+  -> Maybe (f (History action state))
+undo history = do
   (u, us) <- uncons (toUndo history)
-  pure
-    ( history { toUndo = us, toRedo = invertDirected u : toRedo history }
-    , run (unDirected u) subject
-    )
+  pure $ run (unDirected u) (current history) <&> \c -> history
+    { current = c
+    , toUndo  = us
+    , toRedo  = invertDirected u : toRedo history
+    }
 
 redo
-  :: (Runnable action subject m, Invertible action)
-  => History action
-  -> subject
-  -> Maybe (History action, m subject)
-redo history subject = do
+  :: (Functor f, Runnable action state f, Invertible action)
+  => History action state
+  -> Maybe (f (History action state))
+redo history = do
   (r, rs) <- uncons (toRedo history)
-  pure
-    ( history { toRedo = rs, toUndo = invertDirected r : toUndo history }
-    , run (unDirected r) subject
-    )
+  pure $ run (unDirected r) (current history) <&> \c -> history
+    { current = c
+    , toRedo  = rs
+    , toUndo  = invertDirected r : toUndo history
+    }
 
 -- * Test
 
--- test/Komposition/UndoRedoTest.hs
+++ test/Komposition/UndoRedoTest.hs
@@ -13,7 +13,7 @@ import qualified Hedgehog.Range       as Range
 import           Komposition.UndoRedo
 
 data TestAction = SetValue Int Int
-  deriving (Show)
+  deriving (Eq, Show)
 
 instance Invertible TestAction where
   invert (SetValue old new) = SetValue new old
@@ -32,39 +32,59 @@ genTestActions initialOld = do
       genActions new (pred len) (actions <> [Directed (SetValue old new)])
 
 runAndRecordAll
-  :: (Monad m, Invertible action, Runnable action subject m)
-  => (History action, subject)
+  :: (Monad m, Invertible action, Runnable action state m)
+  => History action state
   -> [Directed Forward action]
-  -> m (History action, subject)
-runAndRecordAll =
-  foldM (\(history', state'') action -> runAndRecord action state'' history')
+  -> m (History action state)
+runAndRecordAll = foldM (flip runAndRecord)
 
 applyN
-  :: (Monad m, MonadTest m, Invertible action, Runnable action subject m)
-  => (History action -> subject -> Maybe (History action, m subject))
-  -> (History action, subject)
+  :: (Monad m, MonadTest m, Invertible action, Runnable action state m)
+  => (History action state -> Maybe (m (History action state)))
+  -> (History action state)
   -> Int
-  -> m (History action, subject)
+  -> m (History action state)
 applyN f acc n = foldM
-  (\(history, state') _ -> case f history state' of
-    Just (history', ma) -> (history', ) <$> ma
-    Nothing             -> failure
+  (\history' _ -> case f history' of
+    Just ma -> ma
+    Nothing -> failure
   )
   acc
   (replicate n ())
 
+applyAll
+  :: (Monad m, MonadTest m, Invertible action, Runnable action state m)
+  => (History action state -> Maybe (m (History action state)))
+  -> History action state
+  -> m (History action state)
+applyAll f history =
+  case f history of
+    Just ma -> applyAll f =<< ma
+    Nothing -> pure history
+
 hprop_undo_history_has_correct_number_of_undos = property $ do
   initial <- forAll (Gen.int Range.linearBounded)
   actions <- forAllWith (show . map unDirected) (genTestActions initial)
-  (history, _) <- runAndRecordAll (init, initial) actions
+  history <- runAndRecordAll (init initial) actions
   numUndos history === length actions
 
 hprop_undo_all_returns_initial_state = property $ do
   initial <- forAll (Gen.int Range.linearBounded)
   actions <- forAllWith (show . map unDirected) (genTestActions initial)
-  afterActions <- runAndRecordAll (init, initial) actions
-  annotateShow (snd afterActions)
-  numUndos (fst afterActions) === length actions
-  (_, lastState) <- applyN undo afterActions (numUndos (fst afterActions))
-  annotateShow lastState
-  lastState === initial
+  -- we apply all actions
+  afterActions <- runAndRecordAll (init initial) actions
+  -- then undo them all
+  numUndos afterActions === length actions
+  afterUndos <- applyAll undo afterActions
+  -- and we expect to be back at the initial state
+  current afterUndos === initial
+
+hprop_redo_all_returns_final_state = property $ do
+  initial <- forAll (Gen.int Range.linearBounded)
+  actions <- forAllWith (show . map unDirected) (genTestActions initial)
+  -- we apply all actions
+  afterActions <- runAndRecordAll (init initial) actions
+  -- then undo and redo them all
+  afterRedos <- applyAll undo afterActions >>= applyAll redo
+  -- and we expect to be back at the initial state
+  current afterRedos === current afterActions
